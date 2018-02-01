67e73823e2981e2656a6eef440cc733a7cd1b60a
邓龙 <Bruce.Deng@ubtrobot.com>
2018-02-01 14:04:25 +0800

    组透明调试

--- DLObjectLearningProject/InterView/CoreAnimation/DLLayerTestViewController.m
+++ DLObjectLearningProject/InterView/CoreAnimation/DLLayerTestViewController.m
@@ -17,6 +17,7 @@
 @property (weak, nonatomic) IBOutlet UIView *layerView2;
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
@@ -32,6 +33,8 @@
 //    [self addBluelayerTest];
     [self enablePathShadow];
     [self layerMasking];
+    
+    [self transparentHandle];
 }
 
 - (void)drawingPicture{
@@ -208,14 +211,43 @@
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UIButton *)customButton {
+    //create button
+    CGRect frame = CGRectMake(0, 0, 150, 50);
+    UIButton *button = [[UIButton alloc] initWithFrame:frame];
+    button.backgroundColor = [UIColor whiteColor];
+    button.layer.cornerRadius = 10;
+    
+    //add label
+    frame = CGRectMake(20, 10, 110, 30);
+    UILabel *label = [[UILabel alloc] initWithFrame:frame];
+    label.text = @"Hello World";
+    label.textAlignment = NSTextAlignmentCenter;
+    [button addSubview:label];
+    
+    return button;
 }
-*/
+
+/*
+ 
+ */
+- (void)transparentHandle{
+    //create opaque button
+    UIButton *button1 = [self customButton];
+    button1.center = CGPointMake(100, 30);
+                      [self.containerView addSubview:button1];
+    
+    
+    //create translucent button
+    UIButton *button2 = [self customButton];
+    button2.center = CGPointMake(300,30);
+    button2.alpha = 0.5;
+    [self.containerView addSubview:button2];
+    
+    //enable rasterization for the translucent button
+    //如果UIViewGroupOpacity设置为YES，下面的两行代码就没有作用
+//    button2.layer.shouldRasterize = YES;
+//    button2.layer.rasterizationScale = [UIScreen mainScreen].scale;
+}
 
 @end
