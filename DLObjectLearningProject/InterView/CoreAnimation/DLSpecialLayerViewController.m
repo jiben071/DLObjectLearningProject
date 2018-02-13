2c36aab649868244e1465d44759bed75ae83563b
邓龙 <Bruce.Deng@ubtrobot.com>
2018-02-13 15:26:50 +0800

    增加三维透视示例
    增加三维透视示例

--- DLObjectLearningProject/InterView/CoreAnimation/DLSpecialLayerViewController.m
+++ DLObjectLearningProject/InterView/CoreAnimation/DLSpecialLayerViewController.m
@@ -24,9 +24,13 @@
     // Do any additional setup after loading the view.
     
 //    [self shapeLayerTest];
+    /*
     [self customCorner];
     [self textLayerTest];
     [self customLabel];
+     */
+    
+    [self testCATransformLayer];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -181,6 +185,94 @@
 - (void)customLabel{
     DLLayerLabel *customLabel = [[DLLayerLabel alloc] init];//关键在于+ (Class)layerClass
     customLabel.text = @"";
+}
+
+
+//CATransformLayer
+/*
+ CATransformLayer不同于普通的CALayer，因为它不能显示它自己的内容。只有当存在了一个能作用域子图层的变换它才真正存在。CATransformLayer并不平面化它的子图层，所以它能够用于构造一个层级的3D结构
+ */
+//加颜色
+- (CALayer *)faceWithTransform:(CATransform3D)transform{
+    //create cube face layer
+    CALayer *face = [CALayer layer];
+    face.frame = CGRectMake(-50, -50, 100, 100);
+    
+    //apply a random color
+    CGFloat red = (rand() / (double)INT_MAX);
+    CGFloat green = (rand() / (double)INT_MAX);
+    CGFloat blue = (rand() / (double)INT_MAX);
+    
+    face.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1].CGColor;
+    
+    //apply the tranform and return
+    face.transform = transform;
+    return face;
+}
+
+- (CALayer *)cubeWithTransform:(CATransform3D)transform{
+    //create cube layer
+    CATransformLayer *cube = [CATransformLayer layer];
+    
+    //add cube face 1
+    CATransform3D ct = CATransform3DMakeTranslation(0, 0, 50);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 2
+    ct = CATransform3DMakeTranslation(50, 0, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 3
+    ct = CATransform3DMakeTranslation(0, -50, 0);
+    ct = CATransform3DRotate(ct, M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 4
+    ct = CATransform3DMakeTranslation(0, 50, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 5
+    ct = CATransform3DMakeTranslation(-50, 0, 0);
+    ct = CATransform3DRotate(ct, -M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //add cube face 6
+    ct = CATransform3DMakeTranslation(0, 0, -50);
+    ct = CATransform3DRotate(ct, M_PI, 0, 1, 0);
+    [cube addSublayer:[self faceWithTransform:ct]];
+    
+    //center the cube layer within the contrainer
+    CGSize containerSize = self.containerView.bounds.size;
+    cube.position = CGPointMake(containerSize.width * 0.5, containerSize.height * 0.5);
+    
+    //apply the tranform and return
+    cube.transform = transform;
+    return cube;
+}
+
+- (void)testCATransformLayer{
+    //set up the perspective transform
+    CATransform3D pt = CATransform3DIdentity;
+    pt.m34 = -1.0 / 500.0;
+    self.containerView.layer.sublayerTransform =  pt;
+    
+    //set up the transform for cube 1 and add it
+    CATransform3D c1t = CATransform3DIdentity;
+    c1t = CATransform3DTranslate(c1t, -100, 0, 0);
+    CALayer *cube1 = [self cubeWithTransform:c1t];
+    [self.containerView.layer addSublayer:cube1];
+    
+    
+    //set up the transform for cube2 and add it
+    CATransform3D c2t = CATransform3DIdentity;//首先用 CATransform3DIdentity 来初始化一个 CATransform3D，这个 CATransform3DIdentity 相当于是一个空值。
+    c2t = CATransform3DTranslate(c2t, 100, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 1, 0, 0);
+    c2t = CATransform3DRotate(c2t, -M_PI_4, 0, 1, 0);
+    CALayer *cube2 = [self cubeWithTransform:c2t];
+    [self.containerView.layer addSublayer:cube2];
+    
 }
 
 
