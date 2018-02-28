0a3321dd64aab199d1d640dd5ee851b9ef1ae18c
邓龙 <Bruce.Deng@ubtrobot.com>
2018-02-28 21:14:00 +0800

    增加concat方法解释

--- DLObjectLearningProject/InterView/RAC/Basic/DLRacBasicCourse.m
+++ DLObjectLearningProject/InterView/RAC/Basic/DLRacBasicCourse.m
@@ -81,4 +81,89 @@
     
 }
 
+- (void)mapTest{
+    // 监听文本框的内容改变，把结构重新映射成一个新值.
+    // Map作用:把源信号的值映射成一个新的值
+    // Map使用步骤:
+    // 1.传入一个block,类型是返回对象，参数是value
+    // 2.value就是源信号的内容，直接拿到源信号的内容做处理
+    // 3.把处理好的内容，直接返回就好了，不用包装成信号，返回的值，就是映射的值。
+    // Map底层实现:
+    // 0.Map底层其实是调用flatternMap,Map中block中的返回的值会作为flatternMap中block中的值。
+    // 1.当订阅绑定信号，就会生成bindBlock。
+    // 3.当源信号发送内容，就会调用bindBlock(value, *stop)
+    // 4.调用bindBlock，内部就会调用flattenMap的block
+    // 5.flattenMap的block内部会调用Map中的block，把Map中的block返回的内容包装成返回的信号。
+    // 5.返回的信号最终会作为bindBlock中的返回信号，当做bindBlock的返回信号。
+    // 6.订阅bindBlock的返回信号，就会拿到绑定信号的订阅者，把处理完成的信号内容发送出来。
+    [[_textField.rac_textSignal map:^id _Nullable(NSString * _Nullable value) {
+        //当源信号发出，就会调用这个block，修改源信号的内容
+        //返回值：就是处理完信号的内容
+        return [NSString stringWithFormat:@"输出:%@",value];
+    }] subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+}
+
+/*
+ FlatternMap和Map的区别
+ 1.FlatternMap中的Block返回信号。
+ 2.Map中的Block返回对象。
+ 3.开发中，如果信号发出的值不是信号，映射一般使用Map
+ 4.开发中，如果信号发出的值是信号，映射一般使用FlatternMap。
+ 总结：signalOfsignals用FlatternMap。
+ */
+- (void)flattenMapTest{
+    //创建信号中的信号
+    RACSubject *signalOfSignals = [RACSubject subject];
+    RACSubject *signal = [RACSubject subject];
+    
+    [[signalOfSignals flattenMap:^__kindof RACSignal * _Nullable(id  _Nullable value) {
+        //当signalOfSignals的signals发出信号才会调用
+        return value;
+    }] subscribeNext:^(id  _Nullable x) {
+        //只有signalOfSignals的signal发出信号才会调用，因为内部订阅了bindBlock中返回的信号，也就是flattenMap返回的信号
+        //也就是flattenMap返回的信号发出内容，才会调用
+        NSLog(@"%@aaa",x);
+    }];
+    
+    //信号的信号发送信号
+    [signalOfSignals sendNext:signal];
+    //信号发送内容
+    [signal sendNext:@1];
+}
+
+// ReactiveCocoa操作方法之组合。
+// concat:按一定顺序拼接信号，当多个信号发出的时候，有顺序的接收信号。
+- (void)concatTest{
+    RACSignal *signalA = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        [subscriber sendNext:@1];
+        [subscriber sendCompleted];
+        return nil;
+    }];
+    RACSignal *signalB = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        [subscriber sendNext:@2];
+        return nil;
+    }];
+    
+    //把signalA拼接到signalB后，signalA发送完成，signalB才会被激活
+    RACSignal *concatSignal = [signalA concat:signalB];
+    //以后只需要面对拼接信号开发
+    //订阅拼接的信号，不需要单独订阅signalA,signalB
+    //内部会自动订阅
+    //注意：第一个信号必须发送完成，第二个信号才会被激活
+    [concatSignal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    //concat底层实现
+    //1.当拼接信号被订阅，就会调用拼接信号的didSubscribe
+    //2.didSubscribe中，会先订阅第一个源信号(signalA)
+    //3.会执行第一个源信号(signalA)的didSubscribe
+    //4.第一个原信号(signalA)didSubscribe中发送值，就会调用第一个源信号(signalA)订阅者的nextBlock，通过拼接信号的订阅者把值发出来
+    // 5.第一个源信号（signalA）didSubscribe中发送完成，就会调用第一个源信号（signalA）订阅者的completedBlock,订阅第二个源信号（signalB）这时候才激活（signalB）。
+    // 6.订阅第二个源信号（signalB）,执行第二个源信号（signalB）的didSubscribe
+    // 7.第二个源信号（signalA）didSubscribe中发送值,就会通过拼接信号的订阅者把值发送出来.
+}
+
 @end
