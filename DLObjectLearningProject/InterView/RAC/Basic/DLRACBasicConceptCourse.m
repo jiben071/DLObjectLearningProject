13570217f47d88611eedc8e3c51022d47d5a326b
邓龙 <Bruce.Deng@ubtrobot.com>
2018-03-03 15:15:35 +0800

    RAC基础学习第一弹

--- DLObjectLearningProject/InterView/RAC/Basic/DLRACBasicConceptCourse.m
+++ DLObjectLearningProject/InterView/RAC/Basic/DLRACBasicConceptCourse.m
@@ -13,6 +13,21 @@
 #import "DLCaculator.h"
 #import <ReactiveObjC/ReactiveObjC.h>
 
+@interface FlagItem:NSObject
++ (instancetype)flagWithDict:(NSDictionary *)dict;
+@end
+@implementation FlagItem
+@end
+
+@interface DLRedView:UIView
+@property(nonatomic, weak) UIButton *btn;
+@property(nonatomic, weak) UITextField *textField;
+- (void)btnClick;
+@end
+@implementation DLRedView
+@end
+
+
 /*
  比如按钮的点击使用action，ScrollView滚动使用delegate，属性值改变使用KVO等系统提供的方式。
  其实这些事件，都可以通过RAC处理
@@ -63,6 +78,7 @@
  */
 
 @interface DLRACBasicConceptCourse()
+@property(nonatomic, strong) RACCommand *command;
 @end
 
 @implementation DLRACBasicConceptCourse
@@ -122,5 +138,417 @@
         使用场景一：如果一个信号没被订阅一次，就需要把之前的值重复发送一遍，使用重复提供信息类
         使用场景二：可以设置capacity数量来限制缓存的value的数量，即只缓冲罪行的几个值。
         RACSubject和RACReplaySubject简单使用：
+ 
+
  */
+
+//RACSubject使用步骤
+//1.创建信号 [RACSubject subject],跟RACSignal不一样，创建信号时没有block
+//2.订阅信号 - (RACDisposable *)subscribeNext:(void (^)(id x))nextBlock
+//3.发送信号：sendNext:(id)value
+
+//RACSubject:底层实现和RACSignal不一样
+//1.调用subscribeNext订阅信号，只是把订阅者保存起来，并且订阅者的nextBlock已经赋值了
+//2.调用sendNext发送信号，遍历刚刚保存的所有订阅者，一个一个调用订阅者的nextBlock
+
+- (void)racSubjectTest{
+    //1.创建信号
+    RACSubject *subject = [RACSubject subject];
+    
+    //2.订阅信号
+    [subject subscribeNext:^(id  _Nullable x) {
+        //block调用时刻：当信号发出新值，就会调用
+        NSLog(@"第一个订阅者%@",x);
+    }];
+    
+    [subject subscribeNext:^(id  _Nullable x) {
+        NSLog(@"第二个订阅者%@",x);
+    }];
+    
+    //3.发送信号
+    [subject sendNext:@"1"];
+}
+
+/*
+ // RACReplaySubject使用步骤:
+ // 1.创建信号 [RACSubject subject]，跟RACSiganl不一样，创建信号时没有block。
+ // 2.可以先订阅信号，也可以先发送信号。
+ // 2.1 订阅信号 - (RACDisposable *)subscribeNext:(void (^)(id x))nextBlock
+ // 2.2 发送信号 sendNext:(id)value
+ 
+ // RACReplaySubject:底层实现和RACSubject不一样。
+ // 1.调用sendNext发送信号，把值保存起来，然后遍历刚刚保存的所有订阅者，一个一个调用订阅者的nextBlock。
+ // 2.调用subscribeNext订阅信号，遍历保存的所有值，一个一个调用订阅者的nextBlock
+ 
+ // 如果想当一个信号被订阅，就重复播放之前所有值，需要先发送信号，在订阅信号。
+ // 也就是先保存值，在订阅值。
+ */
+- (void)replaySubjectTest{
+    //1.创建信号
+    RACReplaySubject *replaySubject = [RACReplaySubject subject];
+    
+    //2.发送信号
+    [replaySubject sendNext:@1];
+    [replaySubject sendNext:@2];
+    
+    //3.订阅信号
+    [replaySubject subscribeNext:^(id  _Nullable x) {//调用subscribeNext方法就会在内部创建一个订阅者
+        NSLog(@"第一个订阅者收到的数据%@",x);//每创建一个订阅者，都会接收到之前已经发送过的信号值
+    }];
+    
+    [replaySubject subscribeNext:^(id  _Nullable x) {
+        NSLog(@"第一个订阅者收到的数据%@",x);
+    }];
+}
+
+
+/*
+6.6RACTuple：元组类，类似NSArray，用来包装值
+6.7RACSequence：RAC中的集合类，用于代替NSArray，NSDictionary，可以使用它来快速遍历数组和字典。
+使用场景：1.字典转模型
+RACSequence和RACTuple简单使用
+ */
+- (void)sequenceTest{
+    //1.遍历数组
+    NSArray *numbers = @[@1,@2,@3,@4];
+    // 这里其实是三步
+    // 第一步: 把数组转换成集合RACSequence numbers.rac_sequence
+    // 第二步: 把集合RACSequence转换RACSignal信号类,numbers.rac_sequence.signal
+    // 第三步: 订阅信号，激活信号，会自动把集合中的所有值，遍历出来。
+    [numbers.rac_sequence.signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    //2.遍历字典，遍历出来的键值对会包装秤RACTuple（元组对象）
+    NSDictionary *dict = @{@"name":@"xmg",@"age":@18};
+    [dict.rac_sequence.signal subscribeNext:^(RACTuple  *_Nullable x) {
+        //解包元组，会把元组的值，按顺序给参数里面的变量赋值
+        RACTupleUnpack(NSString *key,NSString *value) = x;
+        NSLog(@"%@ %@",key,value);
+    }];
+    
+    //3.字典转模型
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"flags.plist" ofType:nil];
+    NSArray *dictArray = [NSArray arrayWithContentsOfFile:filePath];
+    NSMutableArray *flags = [NSMutableArray array];
+    //rac_sequence注意点：调用subscribeNext，并不会马上执行nextBlock，而是会等一会
+    [dictArray.rac_sequence.signal subscribeNext:^(id  _Nullable x) {
+        //运用RAC遍历字典，x：字典
+        FlagItem *item = [FlagItem flagWithDict:x];
+        [flags addObject:item];
+    }];
+    
+    //3.3 RAC高级写法
+    //map:映射的意思，目的：把原始值value映射成一个新值
+    //array：把集合转换成数组
+    //底层实现：当信号被订阅，会遍历集合中的原始值
+    NSArray *flagsTwo = [[dictArray.rac_sequence map:^id _Nullable(id  _Nullable value) {
+        return [FlagItem flagWithDict:value];
+    }] array];
+}
+
+/*
+ 6.8 RACCommand：RAC中用于处理事件的类，可以把事件如何处理，事件中的数据如何传递，包装到这个类中，他可以很方便的监控事件的执行过程
+    使用场景：监听按钮点击，网络请求
+ */
+//RACCommand简单使用
+- (void)commandTest{
+    // 一、RACCommand使用步骤:
+    // 1.创建命令 initWithSignalBlock:(RACSignal * (^)(id input))signalBlock
+    // 2.在signalBlock中，创建RACSignal，并且作为signalBlock的返回值
+    // 3.执行命令 - (RACSignal *)execute:(id)input
+    
+    // 二、RACCommand使用注意:
+    // 1.signalBlock必须要返回一个信号，不能传nil.
+    // 2.如果不想要传递信号，直接创建空的信号[RACSignal empty];
+    // 3.RACCommand中信号如果数据传递完，必须调用[subscriber sendCompleted]，这时命令才会执行完毕，否则永远处于执行中。
+    // 4.RACCommand需要被强引用，否则接收不到RACCommand中的信号，因此RACCommand中的信号是延迟发送的。
+    
+    // 三、RACCommand设计思想：内部signalBlock为什么要返回一个信号，这个信号有什么用。
+    // 1.在RAC开发中，通常会把网络请求封装到RACCommand，直接执行某个RACCommand就能发送请求。
+    // 2.当RACCommand内部请求到数据的时候，需要把请求的数据传递给外界，这时候就需要通过signalBlock返回的信号传递了。
+    
+    // 四、如何拿到RACCommand中返回信号发出的数据。
+    // 1.RACCommand有个执行信号源executionSignals，这个是signal of signals(信号的信号),意思是信号发出的数据是信号，不是普通的类型。
+    // 2.订阅executionSignals就能拿到RACCommand中返回的信号，然后订阅signalBlock返回的信号，就能获取发出的值。
+    
+    // 五、监听当前命令是否正在执行executing
+    
+    // 六、使用场景,监听按钮点击，网络请求
+    
+    //1.创建命令
+    RACCommand *command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
+        NSLog(@"执行命令");
+        //创建空信号，必须返回信号
+//        return [RACSignal empty];
+        
+        //2.创建信号，用来传递数据
+        return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+            [subscriber sendNext:@"请求数据"];
+            //注意：数据传递完毕，最好调用sendCompleted，这时命令才执行完毕。
+            [subscriber sendCompleted];
+            return nil;
+        }];
+    }];
+    
+    //强引用命令，不要被销毁，否者接受不到数据
+    _command = command;
+    
+    //3.订阅RACCommand中的信号
+    [command.executionSignals subscribeNext:^(RACSignal  *_Nullable x) {
+        [x subscribeNext:^(id  _Nullable x) {
+            NSLog(@"%@",x);
+        }];
+    }];
+    
+    //RAC高级用法
+    //switchToLatest:用于signal of signals，获取signal of signals发出的最新信号，也就是可以直接拿到RACCommand中的信号
+    [command.executionSignals.switchToLatest subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    //4.监听命令是否执行完毕，默认会来一次，可以直接跳过，skip表示跳过第一次信号
+    [[command.executing skip:1] subscribeNext:^(NSNumber * _Nullable x) {
+        if ([x boolValue] == YES) {
+            //正在执行
+            NSLog(@"正在执行");
+        }else{
+            //执行完成
+            NSLog(@"执行完成");
+        }
+    }];
+    
+    //5.执行命令
+    [self.command execute:@1];
+}
+
+/*
+ 6.9 RACMulticastConnection:用于当一个信号，被多次订阅时，为了保证创建信号时，避免多次调用创建信号中的block，造成副作用，可以使用这个类处理。
+ 使用注意：RACMulticastConnection通过RACSignal的-publish或者-multicast:方法创建
+ RACMulticastConnection简单使用
+ */
+- (void)multicastTest{
+    // RACMulticastConnection使用步骤:
+    // 1.创建信号 + (RACSignal *)createSignal:(RACDisposable * (^)(id<RACSubscriber> subscriber))didSubscribe
+    // 2.创建连接 RACMulticastConnection *connect = [signal publish];
+    // 3.订阅信号,注意：订阅的不在是之前的信号，而是连接的信号。 [connect.signal subscribeNext:nextBlock]
+    // 4.连接 [connect connect]
+    
+    // RACMulticastConnection底层原理:
+    // 1.创建connect，connect.sourceSignal -> RACSignal(原始信号)  connect.signal -> RACSubject
+    // 2.订阅connect.signal，会调用RACSubject的subscribeNext，创建订阅者，而且把订阅者保存起来，不会执行block。
+    // 3.[connect connect]内部会订阅RACSignal(原始信号)，并且订阅者是RACSubject
+    // 3.1.订阅原始信号，就会调用原始信号中的didSubscribe
+    // 3.2 didSubscribe，拿到订阅者调用sendNext，其实是调用RACSubject的sendNext
+    // 4.RACSubject的sendNext,会遍历RACSubject所有订阅者发送信号。
+    // 4.1 因为刚刚第二步，都是在订阅RACSubject，因此会拿到第二步所有的订阅者，调用他们的nextBlock
+    
+    
+    // 需求：假设在一个信号中发送请求，每次订阅一次都会发送请求，这样就会导致多次请求。
+    // 解决：使用RACMulticastConnection就能解决.
+    
+    
+    //1.创建请求信号
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        NSLog(@"发送请求");
+        return nil;
+    }];
+    
+    //2.订阅信号
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"接收数据");
+    }];
+    
+    //2.订阅信号
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"接收数据");
+    }];
+    
+    //3.运行结果，会执行两遍发送请求，也就是每次订阅都会发送一次请求
+    
+    
+    //RACMulticastConnection：解决重复请求问题
+    //1.创建信号
+    RACSignal *signalAgain = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        NSLog(@"发送请求");
+        [subscriber sendNext:@1];
+        return nil;
+    }];
+    
+    //2.创建连接
+    RACMulticastConnection *connect = [signalAgain publish];
+    
+    //3.订阅信号
+    //注意：订阅信号，也不能激活信号，只是保存订阅者到数组，必须通过调用连接，就会一次性调用所有订阅者的sendNext：
+    [connect.signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"订阅者一信号");
+    }];
+    [connect.signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"订阅者二信号");
+    }];
+    
+    //4.连接，激活信号
+    [connect connect];
+}
+
+/*
+ 6.10 RACSchedule:RAC中的队列，用GCD封装。
+ 6.11 RACUnit：表示stream不包含有意义的值，也就是看到这个可以直接理解为nil
+ 6.11 RACEvent:把数据包装成信号事件(signal event)。它主要通过RACSignal的-materialize来使用，然并卵
+ */
+
+/*
+ 7.ReactiveCocoa开发中常见用法
+ 7.1代替代理
+ rac_signalForSelector:用于替代代理
+ 7.2代替KVO：
+ rac_valuesAndChangesForKeyPath:用于监听某个对象的属性改变
+ 7.3监听事件
+ rac_signalForControlEvents:用于监听某个事件
+ 7.4代替通知
+ rac_addObserverForName:用于监听某个通知
+ 7.5监听文本框文字改变
+ rac_textSignal:只要文本框发出改变就会发出这个信号
+ 7.6处理当界面有多次请求时，需要都获取到数据时，才能展示界面
+ rac_liftSelector:withSignalsFromArraySignals:当传入的Signals（信号数组），每一个signal都至少sendNext过一次，就会去触发第一个selector参数的方法
+ 使用注意：几个信号，参数一的方法就几个参数，每个参数对应信号发出的数据。
+ */
+
+- (void)basicTest{
+    DLRedView *redV = [[DLRedView alloc] init];
+    //1.代替代理
+    // 需求：自定义redView,监听红色view中按钮点击
+    // 之前都是需要通过代理监听，给红色View添加一个代理属性，点击按钮的时候，通知代理做事情
+    // rac_signalForSelector:把调用某个对象的方法的信息转换成信号，就要调用这个方法，就会发送信号。
+    // 这里表示只要redV调用btnClick:,就会发出信号，订阅就好了。
+    [[redV rac_signalForSelector:@selector(btnClick)] subscribeNext:^(RACTuple * _Nullable x) {
+        NSLog(@"点击红色按钮");
+    }];
+    
+    //2.KVO
+    // 把监听redV的center属性改变转换成信号，只要值改变就会发送信号
+    // observer:可以传入nil
+    [[redV rac_valuesAndChangesForKeyPath:@"center" options:NSKeyValueObservingOptionNew observer:nil] subscribeNext:^(RACTwoTuple<id,NSDictionary *> * _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+    
+    //3.监听事件
+    //把按钮点击事件转换为信号，点击按钮，就会发送信号
+    [[redV.btn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"按钮被点击了");
+    }];
+    
+    
+    //4.代替通知
+    [[[NSNotificationCenter defaultCenter] rac_addObserverForName:UIKeyboardWillShowNotification object:nil] subscribeNext:^(NSNotification * _Nullable x) {
+        NSLog(@"键盘弹出");
+    }];
+    
+    //5.监听文本框的文字改变
+    [redV.textField.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
+        NSLog(@"文字改变了%@",x);
+    }];
+    
+    //6.处理多个请求，都返回结果的时候，统一做处理
+    RACSignal *request1 = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        //发送请求1
+        [subscriber sendNext:@"发送请求"];
+        return nil;
+    }];
+    
+    RACSignal *request2 = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        //发送请求2
+        [subscriber sendNext:@"发送请求2"];
+        return nil;
+    }];
+    
+    //使用注意：几个信号，参数一的方法就几个参数，每个参数对应信号发出的数据。
+    [self rac_liftSelector:@selector(updateWithR1:R2:) withSignalsFromArray:@[request1,request2]];
+}
+
+//更新UI
+- (void)updateWithR1:(NSString *)value1 R2:(NSString *)value2{
+    
+}
+
+/*
+ 8.ReactiveCocoa常见宏
+ 8.1 RAC(TARGET,[KEYPATH,[NIL_VAlUE]])：用于给某个对象的某个属性绑定。
+ 
+ 8.2 RACObserver(self,name):监听某个对象的某个属性，返回的是信号
+ */
+- (void)macroTest{
+    DLRedView *redV = [[DLRedView alloc] init];
+    //只要文本框文字改变，就会修改label的文字
+    RAC(redV.btn.titleLabel,text) = redV.textField.rac_textSignal;
+    
+    [RACObserve(redV, center) subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+}
+
+/*
+ 8.3  @weakify(Obj)和@strongify(Obj),一般两个都是配套使用,在主头文件(ReactiveCocoa.h)中并没有导入，需要自己手动导入，RACEXTScope.h才可以使用。但是每次导入都非常麻烦，只需要在主头文件自己导入就好了。
+ */
+
+/*
+ 8.4 RACTuplePack：把数据包装成RACTuple（元组类）
+ 8.5 RACTupleUnpack：把RACTuple（元组类）解包成对应的数据。
+ */
+- (void)tupleTest{
+    //把参数中的数据包装成元组
+    RACTuple *tuple = RACTuplePack(@"xmg",@20);
+    
+    //解包元组，会把元组的值，按顺序给参数里面的变量赋值
+    RACTupleUnpack(NSString *name,NSNumber *age) = tuple;
+}
+
+
 @end
+
+
+//RACSubject替换代理
+//需求：
+//1.给当前控制器添加一个按钮，modal到另一个控制器界面
+//2.另一个控制器view中有个按钮，点击按钮，通知当前控制器
+
+@interface DLTwoViewController:UIViewController
+@property(nonatomic, strong) RACSubject *delegateSignal;
+@end
+
+@implementation DLTwoViewController
+- (IBAction)notice:(id)sender{
+    //通知第一个控制器，告诉它，按钮被点了
+    //通知代理
+    //判断代理信号是否有值
+    if (self.delegateSignal) {
+        //有值，才需要通知
+        [self.delegateSignal sendNext:nil];
+    }
+}
+@end
+
+@interface DLOneViewController:UIViewController
+@end
+
+@implementation DLOneViewController
+- (IBAction)btnClick:(id)sender{
+    //创建第二个控制器
+    DLTwoViewController *twoVC = [[DLTwoViewController alloc] init];
+    
+    //设置代理信号
+    twoVC.delegateSignal = [RACSubject subject];
+    
+    //订阅代理信号
+    [twoVC.delegateSignal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"点击了通知按钮");
+    }];
+    
+    //跳转到第二个控制器
+    [self presentViewController:twoVC animated:YES completion:nil];
+}
+@end
+
+
+
+
