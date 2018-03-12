44f26ec5990278c534338bdfcc2a7d7528c02156
邓龙 <Bruce.Deng@ubtrobot.com>
2018-03-12 14:24:06 +0800

    RAC重复订阅概念纠正

--- DLObjectLearningProject/InterView/RAC/Basic/DLSubscriptionComprehandViewController.m
+++ DLObjectLearningProject/InterView/RAC/Basic/DLSubscriptionComprehandViewController.m
@@ -96,4 +96,327 @@
     }];
 }
 
+
+#pragma mark - replay, replayLast, and replayLazily  区分学习
+//问题所在，重复订阅，目的只需执行一次
+- (void)reExecutedExample{
+    __block int num = 0;
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        num++;
+        NSLog(@"Increment num to: %@",@(num));
+        [subscriber sendNext:@(num)];
+        return nil;
+    }];
+    
+    NSLog(@"start subscriptions");
+    
+    //In this way, a normal RACSignal can be thought of as lazy, as it doesn’t do any work until it has a subscriber.
+    //有点类似懒加载，直到有订阅者订阅它
+    //Subscriber 1 (S1)
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S1:%@",x);
+    }];
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S2:%@",x);
+    }];
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S3:%@",x);
+    }];
+}
+
+//Our second example shows how each subscriber only receives the values that are sent after their subscription is added.
+- (void)problemTwoTest{
+    RACSubject *letters = [RACSubject subject];
+    RACSignal *signal = letters;
+    
+    NSLog(@"Subscribe S1");
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S1: %@", x);
+    }];
+    
+    NSLog(@"Send A");
+    [letters sendNext:@"A"];
+    NSLog(@"Send B");
+    [letters sendNext:@"B"];
+    
+    NSLog(@"Subscribe S2");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S2: %@", x);
+    }];
+    
+    NSLog(@"Send C");
+    [letters sendNext:@"C"];
+    NSLog(@"Send D");
+    [letters sendNext:@"D"];
+    
+    NSLog(@"Subscribe S3");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+}
+
+//replay test
+- (void)replayTest{
+    __block int num = 0;
+    RACSignal *signal = [[RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        num++;
+        NSLog(@"Increament num to: %i",num);
+        [subscriber sendNext:@(num)];
+        return nil;
+    }] replay];
+    
+    NSLog(@"Start subscriptions");
+    
+    // Subscriber 1 (S1)
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S1: %@", x);
+    }];
+    
+    // Subscriber 2 (S2)
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S2: %@", x);
+    }];
+    
+    // Subscriber 3 (S3)
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+    
+    
+    /*
+     打印结果：（没有重复执行num++，打印的是历史signal的值）
+     This time the num integer is incremented immediately, before there are even any subscribers. And it is only incremented once, meaning that the subscription code is only been executed a single time, regardless of how many subscribers the signal has.
+     Increment num to: 1
+     Start subscriptions
+     S1: 1
+     S2: 1
+     S3: 1
+     */
+    
+}
+
+//The second example shows how each new subscriber receives the full history of the signal:
+- (void)fullHistoryTest{
+    RACSubject *letters = [RACSubject subject];
+    RACSignal *signal = [letters replay];
+    
+    NSLog(@"Subscribe S1");
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S1:%@",x);
+    }];
+    NSLog(@"Send A");
+    [letters sendNext:@"A"];
+    NSLog(@"Send B");
+    [letters sendNext:@"B"];
+    
+    NSLog(@"Subscribe S2");
+    [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"S2:%@",x);
+    }];
+    
+    NSLog(@"Send C");
+    [letters sendNext:@"C"];
+    NSLog(@"Send D");
+    [letters sendNext:@"D"];
+    
+    NSLog(@"Subscribe S3");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+    
+    /*打印结果：
+     Subscribe S1
+     
+     Send A
+     S1: A
+     
+     Send B
+     S1: B
+     
+     Subscribe S2
+     S2: A
+     S2: B
+     
+     Send C
+     S1: C
+     S2: C
+     
+     Send D
+     S1: D
+     S2: D
+     
+     Subscribe S3
+     S3: A
+     S3: B
+     S3: C
+     S3: D
+     
+     */
+}
+
+- (void)replayLatestTest{
+    RACSubject *letters = [RACSubject subject];
+    RACSignal *signal = [letters replayLast];
+    
+    NSLog(@"Subscribe S1");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S1: %@", x);
+    }];
+    
+    NSLog(@"Send A");
+    [letters sendNext:@"A"];
+    NSLog(@"Send B");
+    [letters sendNext:@"B"];
+    
+    NSLog(@"Subscribe S2");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S2: %@", x);
+    }];
+    
+    NSLog(@"Send C");
+    [letters sendNext:@"C"];
+    NSLog(@"Send D");
+    [letters sendNext:@"D"];
+    
+    NSLog(@"Subscribe S3");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+    
+    /*打印结果：
+     Subscribe S1
+     
+     Send A
+     S1: A
+     
+     Send B
+     S1: B
+     
+     Subscribe S2
+     S2: B
+     
+     Send C
+     S1: C
+     S2: C
+     
+     Send D
+     S1: D
+     S2: D
+     
+     Subscribe S3
+     S3: D
+     */
+}
+
+- (void)lazilyRepeatTest{
+    __block int num = 0;
+    RACSignal *signal = [[RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        num++;
+        NSLog(@"Increment num to:%@",@(num));//这里不会马上执行，直到第一次订阅
+        [subscriber sendNext:@(num)];
+        return nil;
+    }] replayLazily];
+    
+    NSLog(@"Start subscriptions");
+    
+    // Subscriber 1 (S1)
+    [signal subscribeNext:^(id x) {//直到第一次订阅
+        NSLog(@"S1: %@", x);
+    }];
+    
+    // Subscriber 2 (S2)
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S2: %@", x);
+    }];
+    
+    // Subscriber 3 (S3)
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+    
+    /*
+     打印结果：
+     Start subscriptions
+     Increment num to: 1
+     S1: 1
+     S2: 1
+     S3: 1
+     */
+}
+
+//And the second example shows that the full history is sent to any new subscribers, just like with -replay.
+-(void)replayLazilyTest2{
+    RACSubject *letters = [RACSubject subject];
+    RACSignal *signal = [letters replayLazily];
+    
+    NSLog(@"Subscribe S1");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S1: %@", x);
+    }];
+    
+    NSLog(@"Send A");
+    [letters sendNext:@"A"];
+    NSLog(@"Send B");
+    [letters sendNext:@"B"];
+    
+    NSLog(@"Subscribe S2");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S2: %@", x);
+    }];
+    
+    NSLog(@"Send C");
+    [letters sendNext:@"C"];
+    NSLog(@"Send D");
+    [letters sendNext:@"D"];
+    
+    NSLog(@"Subscribe S3");
+    [signal subscribeNext:^(id x) {
+        NSLog(@"S3: %@", x);
+    }];
+    
+    /*
+     打印结果：
+     Subscribe S1
+     
+     Send A
+     S1: A
+     
+     Send B
+     S1: B
+     
+     Subscribe S2
+     S2: A
+     S2: B
+     
+     Send C
+     S1: C
+     S2: C
+     
+     Send D
+     S1: D
+     S2: D
+     
+     Subscribe S3
+     S3: A
+     S3: B
+     S3: C
+     S3: D
+     */
+}
+
+/*
+ Summary:
+ ReactiveCocoa provides three convenience methods for allowing multiple subscribers to the same signal,without re-executing the source signal's subscrption code, and to provide some level of historical values to later subscribers.
+ -replay and replayLast both make the signal hot,and will provide either all values(-replay) or the most recent(-replayLast) value to subscribers.-replayLazily returns a cold signal that will provide all of the signal's values to subscribers.
+ */
+
+
+
+
+
+
+
+
+
+
 @end
