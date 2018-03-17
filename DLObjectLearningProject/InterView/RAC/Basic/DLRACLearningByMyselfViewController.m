aa7c95afba55104c9f472efa84830f595c206d1d
jiben071 <919031029@qq.com>
2018-03-17 23:25:04 +0800

    RAC学习

--- DLObjectLearningProject/InterView/RAC/Basic/DLRACLearningByMyselfViewController.m
+++ DLObjectLearningProject/InterView/RAC/Basic/DLRACLearningByMyselfViewController.m
@@ -24,7 +24,8 @@
     
     //    [self flattemMapTest2];
 //    [self mapTest];
-    [self signalTest];
+//    [self signalTest];
+    [self subjectTest];
 }
 
 
@@ -146,7 +147,8 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     //    [self flattemMapTest2];
 //    [self mapTest];
-    [self signalTest];
+//    [self signalTest];
+    [self subjectTest];
 }
 
 #pragma mark - RACSignal
@@ -202,5 +204,33 @@
  pthread_mutex_unlock() 与 pthread_mutex_lock() 相配合，它把线程已经加锁的互斥对象解锁。
  */
 
+#pragma mark - RACSubject
+/*
+ RACSubject
+ 
+ RACSubject 在使用中我们可以完全代替代理，代码简介方法。具体代码请看demo中的RACSubject。
+ 
+ 总结
+ 
+ 我们完全可以用RACSubject代替代理/通知，确实方便许多 这里我们点击TwoViewController的pop的时候 将字符串"ws"传给了ViewController的button的title。
+ */
+- (void)subjectTest{
+    //1.创建信号
+    RACSubject *subject = [RACSubject subject];
+    
+    //2.订阅信号
+    [subject subscribeNext:^(id  _Nullable x) {
+        //block：当有数据发出的时候就会调用
+        //block:处理数据
+        NSLog(@"%@",x);
+    }];
+    
+    //3.发送信号
+    [subject sendNext:@2];
+    
+    /*
+     注意 RACSubject和RACReplaySubject的区别 RACSubject必须要先订阅信号之后才能发送信号， 而RACReplaySubject可以先发送信号后订阅. RACSubject 代码中体现为：先走TwoViewController的sendNext，后走ViewController的subscribeNext订阅 RACReplaySubject 代码中体现为：先走ViewController的subscribeNext订阅，后走TwoViewController的sendNext 可按实际情况各取所需。
+     */
+}
 
 @end
